04ae50ce5e8591a50fde8cec6ae910629cb44fbc
Émilie Tossan <emilietossan@MacBook-Pro-de-Emilie.home>
2022-01-19 02:38:47 +0100

    Image

--- src/Controller/Admin/AdminImageController.php
+++ src/Controller/Admin/AdminImageController.php
@@ -71,7 +71,8 @@ class AdminImageController extends AbstractController
     public function adminUpdateImage(
         $id, 
         ImageRepository $imageRepository, 
-        Request $request, 
+        Request $request,
+        SluggerInterface $sluggerInterface,
         EntityManagerInterface $entityManagerInterface)
     {
         $image = $imageRepository->find($id);
@@ -81,7 +82,28 @@ class AdminImageController extends AbstractController
         $imageForm->handleRequest($request);
 
         if($imageForm->isSubmitted() && $imageForm->isValid()){
+
+            $imageFile = $imageForm->get('src')->getData();
+
+            if ($imageFile){
+
+                $originalFileName = pathinfo($imageFile->getClientOriginalName(), PATHINFO_FILENAME);
+
+                $safeFileName = $sluggerInterface->slug($originalFileName);
+
+                $newFileName = $safeFileName . '-' . uniqid() . '.' . $imageFile->guessExtension();
+
+                $imageFile->move(
+                    $this->getParameter('images_directory'),
+                    $newFileName
+                );
+
+                $image->setSrc($newFileName);
+
+            }
+            
             $entityManagerInterface->persist($image);
+            
             $entityManagerInterface->flush();
 
             return $this->redirectToRoute("admin_image_list");
