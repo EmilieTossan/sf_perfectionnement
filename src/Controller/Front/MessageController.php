901077386e30c2a806e3b1d5a431a10199405458
Émilie Tossan <emilietossan@MacBook-Pro-de-Emilie.home>
2022-01-20 04:18:50 +0100

    User

---
+++ src/Controller/Front/MessageController.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Controller\Front;
+
+use Symfony\Bridge\Twig\Mime\TemplatedEmail;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Mailer\MailerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+class MessageController extends AbstractController
+{
+    public function contact()
+    {
+        return $this->render('front/contact.html.twig');
+    }
+
+    public function message(Request $request, MailerInterface $mailerInterface)
+    {
+        $message = $request->request->get('message');
+
+        $email = (new TemplatedEmail())
+            ->from('dd@test.com')
+            ->to('gg@test.com')
+            ->subject('message')
+            ->htmlTemplate('front/email.html.twig')
+            ->context(['message' => $message]);
+
+            $mailerInterface->send($email);
+
+            return $this->redirectToRoute('contact');
+    }
+}
